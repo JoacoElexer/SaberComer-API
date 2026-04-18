5e7e15ed87077d87d9be2ea4311c60e70c985efb
Joaco_Vrz <joacoe.vrz@gmail.com>
2026-04-17 21:44:22 -0600

    cors añadido

--- server.js
+++ server.js
@@ -5,9 +5,29 @@ import mongoConnection from './DB/MongoDbConfig.js';
 import router from './router/router.js';
 import swaggerUi from 'swagger-ui-express';
 import swaggerSpec from './config/swagger.js';
+import cors from 'cors';
 
 dotenv.config();
+
+const webAppUrl = process.env.WEBAPP_URL
+const PORT = process.env.PORT || 3010;
+
+const whiteList = [
+    webAppUrl,
+    'http://localhost:5173'
+];
+
 const app = express();
+
+app.use(cors({
+    origin: function (origin, callback) {
+        if (!origin || whiteList.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error('No permitido por CORS'));
+        }
+    }
+}));
 
 // !! Conexión a la base de datos
 try {
@@ -27,10 +47,8 @@ router(app);
 // Manejo de errores
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 3010;
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en el puerto ${PORT}`);
     console.log(`Endpoint general: http://localhost:${PORT}/fichaPacientes`);
-
     console.log(`Documentacion Swagger: http://localhost:${PORT}/api-docs`);
 });
